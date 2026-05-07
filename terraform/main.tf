798b3879c3f8fa0aba2a3f620914712564d11dd0
bhavyaerjxvaq <bhavya@ibm.com>
2026-05-07 16:34:40 +0000

    feat: add VPC resource (app-prod2-vpc, 10.0.0.0/16)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,27 @@
 # Terraform configuration - initial deployment
 terraform {
   required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "app-prod2-vpc"
+    Environment = "prod"
+    ManagedBy   = "terraform"
+  }
 }
